df092816e40b1e3089ce1da3837fbf2ce73f698a
John Abramo <jmabramo@buffalo.edu>
2022-11-30 19:00:48 -0500

    [REM] logging to console

--- view/src/router/index.js
+++ view/src/router/index.js
@@ -39,7 +39,6 @@ router.beforeEach((to, from, next) => {
       else next("/");
     })
     .catch(() => {
-      console.log(to.name);
       if (to.name !== "login" && to.name !== "register") next("login");
       else next();
     });
