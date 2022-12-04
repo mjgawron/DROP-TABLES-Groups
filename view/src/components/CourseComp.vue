4ce54f29e5174a03048068b2262cd7aba4d80ecf
Jessica Breuhaus <jessica.breuhaus@gmail.com>
2022-12-04 16:01:06 -0500

    Added CourseComp.vue

---
+++ view/src/components/CourseComp.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="course">
+    <div class="info">
+      <h2>{{ courseName }}</h2>
+      <h4>{{ instructor }}</h4>
+    </div>
+    <div class="link">
+      <a href="/">{{ action }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CourseComp",
+  props: {
+    courseName: String,
+    instructor: String,
+    action: String,
+  },
+};
+</script>
+
+<style scoped>
+.course {
+  width: 60%;
+  padding: 20px;
+  margin: 20px auto;
+  border: 2px solid black;
+  border-radius: 8px;
+}
+.info {
+  text-align: left;
+}
+.link {
+  text-align: right;
+}
+</style>
