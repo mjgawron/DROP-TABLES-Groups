626d4129719a71bd470c13c3d287c93aa851d6f2
Milau02 <michaellaufer2@gmail.com>
2022-12-04 18:06:45 -0500

    added routing for questions

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from views.course import course
+from views.questions import question
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ if app.debug:
 
 # Register each blueprint with its prefix
 app.register_blueprint(course, url_prefix="/api/course")
-
+app.register_blueprint(question, url_prefix="/api/question")
 
 
 if __name__ == '__main__':
