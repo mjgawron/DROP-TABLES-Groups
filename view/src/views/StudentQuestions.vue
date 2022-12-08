5f7b8173644e47703edf0859790450ac2c0b49bc
Jessica Breuhaus <jessica.breuhaus@gmail.com>
2022-12-08 15:30:15 -0500

    Fixed question related componenets and added StudentQuestions view

---
+++ view/src/views/StudentQuestions.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="studentquestions">
+    <CourseTabs />
+    <div class="container">
+      <QuestionList :questions="questions" />
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+import CourseTabs from "../components/CourseTabs.vue";
+import QuestionList from "../components/QuestionList.vue";
+
+export default {
+  name: "StudentQuestions",
+  data() {
+    return {
+      questions: [{}, {}, {}, {}],
+    };
+  },
+  props: {
+    course_id: Number,
+  },
+  components: {
+    CourseTabs,
+    QuestionList,
+  },
+  //   beforeMount() {
+  //     axios.get("/course/" + this.course_id + "/questions").then((response) => {
+  //       this.questions = response.data
+  //     });
+  //   }
+};
+</script>
+
+<style scoped></style>
