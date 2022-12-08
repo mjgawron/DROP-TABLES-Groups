3bf400cc5b4e1999060b97e4a5347a449b83883c
Milau02 <michaellaufer2@gmail.com>
2022-12-08 17:54:55 -0500

    completed the route for grabbing grades for instructors and students, offloaded submission creation to the database to be compatbile with websocket code

--- api/database/submission.py
+++ api/database/submission.py
@@ -1,8 +1,27 @@
 from database.database import db, get_next_id
+from database.questions import read_question, read_question_ids_course
+from database.course import is_member, is_enrolled, is_instructor, read_course
+from database.account import get_user_by_id
 
 def create_submission(data):
     id = get_next_id("submissions")
-    internal_data = {**data,"id":id, "deleted":False}
+    
+    user_id = data["user_id"]    
+    student_choice = data["choice"]
+    question_id = data["question_id"]
+    
+    user:dict = get_user_by_id(user_id)
+
+    question:dict = read_question(question_id)
+    course_id = question.get("course_id")
+    correct_choice = question.get("answer")
+    
+    correct = student_choice == correct_choice
+
+    save_data = {"course_id":course_id,"question_id":question_id,"user_id":user_id,"user_name":user.get("name"),"correct":correct}
+    
+
+    internal_data = {**save_data,"id":id, "deleted":False}
     db["submissions"].insert_one(internal_data)
     return read_submission(id)
 
@@ -13,6 +32,14 @@ def read_submission(submission_id):
 def read_submission_course(course_id:int):
     result = db["submissions"].find({"deleted":False,"course_id":course_id},{"_id": 0, "deleted": 0})
     return list(result)
+
+def read_submission_student(user_id,question_id):
+    result = db["submissions"].find_one({"deleted":False,"user_id":user_id,"question_id":question_id},{"_id":0,"deleted":0})
+    return result
+
+def already_submitted(user_id,question_id):
+    result = db["submissions"].find_one({"deleted":False,"question_id":question_id,"user_id":user_id})
+    return result != None
 
 def read_user_submissions(user_id):
     result = db["submissions"].find({"deleted":False,"user_id":user_id},{"_id":0,"deleted":0})
--- api/views/submission.py
+++ api/views/submission.py
@@ -1,20 +1,52 @@
 import json
 from flask import Blueprint, request, abort
-from database.submission import list_submission, read_submission, read_submission_course, create_submission, delete_submission, update_submission, read_user_submissions
-from views.account import get_user_by_token
+from database.submission import list_submission, read_submission, read_submission_course, create_submission, delete_submission, update_submission, read_user_submissions, already_submitted, read_submission_student
+from database.questions import read_question, read_question_ids_course
+from database.course import is_member, is_enrolled, is_instructor, read_course
+from database.account import get_user_by_token
 
 submission = Blueprint("submission",__name__)
 
-@submission.route("",methods=["POST"])
-def post_submission_route():
-    data = request.json
-    question_id = data["question_id"]
-
-    token = request.cookies.get("auth")
-    user:dict = get_user_by_token(token)
-
-
 @submission.route("/<int:course_id>/grades",methods=["GET"])
-def get_course_grades():
-    #if instructor returns list of dicts [{student_id: {question_id: correctness}}]
-    return
+def get_course_grades(course_id):
+    #if instructor returns list of dicts [{student_id: id, score_list: {question_id: correctness}} . . .]
+    #else returns list of singular dict  [{student_id: id, score_list: {question_id: correctness}} . . .] for that student
+    user:dict = get_user_by_token(request.cookies.get("auth"))
+    user_id = user.get("id")
+
+
+    if not is_member(user_id,course_id):
+        abort(400)
+        
+    instructor_status = is_instructor(user_id,course_id)
+    course:dict = read_course(course_id)
+
+    if instructor_status:
+        roster = course.get("students")
+        question_ids = read_question_ids_course(course_id)
+        gradeList = []
+        for student_id in roster:
+            score_list = []
+            for question_id in question_ids:
+                chosen_correctly = False
+                if already_submitted(student_id,question_id):
+                    student_submission:dict = read_submission_student(student_id,question_id)
+                    chosen_correctly = student_submission.get("correct")
+                score_list.append({"correctness":chosen_correctly,"question_id":question_id})
+            gradeList.append({"student_id":student_id,"score_list":score_list})
+        return gradeList
+    
+    else:
+        student_id = user_id
+        question_ids = read_question_ids_course(course_id)
+        gradeList = []
+        
+        score_list = []
+        for question_id in question_ids:
+            chosen_correctly = False
+            if already_submitted(student_id,question_id):
+                student_submission:dict = read_submission_student(student_id,question_id)
+                chosen_correctly = student_submission.get("correct")
+            score_list.append({"correctness":chosen_correctly,"question_id":question_id})
+        gradeList.append({"student_id":student_id,"score_list":score_list})
+        return gradeList
