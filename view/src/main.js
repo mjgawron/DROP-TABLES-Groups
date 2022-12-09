9830f8d21d14d3c42e94600a9d463a71d0f3a853
John Abramo <jmabramo@buffalo.edu>
2022-12-09 16:46:02 -0500

    change for deployment

--- view/src/main.js
+++ view/src/main.js
@@ -3,6 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080/api";
+axios.defaults.baseURL = "http://jesseballcap.com/api";
 
 createApp(App).use(router).mount("#app");
