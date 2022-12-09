e73d119f1bbe453079fb4294888492353222c229
mattgron <mattygron@gmail.com>
2022-12-08 19:35:42 -0500

    GradeComp, GradeList, GradeView

--- view/src/components/GradeComp.vue
+++ view/src/components/GradeComp.vue
@@ -3,14 +3,14 @@
     <h3>{{ grade.name }}</h3>
     <table class="table">
       <tr class="question_ids">
-        <th :key="answer.question_id" v-for="answer in grade.answers">
-          <h4>Q{{ answer.question_id }},</h4>
+        <th :key="idx" v-for="idx in this.questionIncrement">
+          <h4>Q{{ idx + 1 }}---</h4>
         </th>
-        <th>Tot:</th>
+        <th>Total:</th>
       </tr>
       <tr class="answers">
-        <td :key="answer.question_id" v-for="answer in grade.answers">
-          {{ answer.answer }}
+        <td :key="score.question_id" v-for="score in grade.score_list">
+          {{ score.correctness }}
         </td>
         <td>
           {{ score }}
@@ -26,6 +26,7 @@ export default {
   name: "GradeComp",
   data() {
     return {
+      questionIncrement: 0,
       score: 0,
     }
   },
@@ -34,12 +35,13 @@ export default {
   },
   beforeMount() {
       var correct = 0;
-      const numQuestions = this.grade.answers.length;
-      for (let answer of this.grade.answers) {
-        if (answer.answer === "t") {
+      const numQuestions = this.grade.score_list.length;
+      for (let score of this.grade.score_list) {
+        if (score.correctness) {
           correct += 1;
         }
       }
+      this.questionIncrement = Array.from(Array(numQuestions).keys())
       this.score = Intl.NumberFormat('default', {style: 'percent', minimumFractionDigits: 2, maximumFractionDigits: 2,}).format((correct / numQuestions));
     },
 };
